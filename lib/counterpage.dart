199637af4badb08dff86b5919485512c3666fd53
trekhub <teddyomondi001@gmail.com>
2023-03-06 17:42:49 +0300

    fix:fixed the counterprovider to accept arguments

--- lib/counterpage.dart
+++ lib/counterpage.dart
@@ -1,11 +1,64 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
-class CounterPage extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_tut/main.dart';
+
+class CounterPage extends ConsumerWidget {
   const CounterPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<int> counter = ref.watch(counterProvider(5));
+
+    // ref.listen<int>(counterProvider, (previous, next){
+    //   if(next == 5){
+    //     dialogMethod(context);
+    //   }
+    // });
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Page'),
+        // actions: [
+        //   IconButton(onPressed: () {
+        //     ref.refresh(counterProvider);
+        //   }, icon: const Icon(Icons.refresh_outlined)),
+        //   const SizedBox(width: 10,)
+
+        // ],
+      ),
+      body: Center(
+        child: Text(
+          counter
+              .when(
+                  data: (int value) => value,
+                  error: (Object e, _) => e,
+                  loading: () => 0)
+              .toString(),
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+      //   floatingActionButton: FloatingActionButton(
+      //     onPressed: () {
+      //       ref.read(counterProvider.notifier).state++;
+
+      //     },
+      //     child: const Icon(Icons.add),
+      //   )
+    );
   }
+
+  // Future<dynamic> dialogMethod(BuildContext context) {
+  //   return showDialog(context: context, builder: (context){
+  //       return AlertDialog(
+  //         title: const Text('Counter Warning'),
+  //         content: const Text('Counter has reached 5'),
+  //         actions: [
+  //           TextButton(onPressed: () {
+  //             Navigator.of(context).pop();
+  //           }, child: const Text('OK'))
+  //         ],
+  //       );
+  //     });
+  // }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_tut/homepage.dart';
 
-final counterProvider = StateProvider((ref) => 0);
+abstract class WebsocketClient {
+  Stream<int> getCounterStream(int start);
+}
+
+class FakeWebsocketClient implements WebsocketClient {
+  @override
+  Stream<int> getCounterStream([int start = 0]) async* {
+    int i = start;
+    while (true) {
+      await Future.delayed(const Duration(milliseconds: 500));
+      yield i++;
+    }
+  }
+}
+
+final websocketClientProvider = Provider<WebsocketClient>((ref) {
+  return FakeWebsocketClient();
+});
+
+final counterProvider = StreamProvider.family<int, int>((ref, start) {
+  final wsClient = ref.watch(websocketClientProvider);
+
+  return wsClient.getCounterStream(start);
+});
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -14,20 +38,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.greenAccent,
-          brightness: Brightness.dark,
-          surface: Colors.white,
+        title: 'Counter App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.greenAccent,
+            brightness: Brightness.dark,
+            surface: Colors.green,
+          ),
+          primarySwatch: Colors.blue,
         ),
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('RiverPod'),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: const HomePage());
   }
 }
